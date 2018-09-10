a9216b5056a0624779cff03b136221f3e506c3fd
Julie K <julieskim160@gmail.com>
2018-09-10 09:39:31 -0700

    initialize components

--- src/components/address-book.js
+++ src/components/address-book.js
@@ -3,3 +3,7 @@ import React from 'react';
 import Contact from './contact';
 
 // The AddressBook component goes here.  It should be the default export.
+
+export default class AddressBook extends React.Component {
+    
+}
--- src/components/contact.js
+++ src/components/contact.js
@@ -2,4 +2,10 @@ import React from 'react';
 
 import './contact.css';
 
-// The Contact component goes here.  It should be the default export.
+
+//stateless
+export default function Contact() {
+    //three props: name, photo, address
+
+} 
+
