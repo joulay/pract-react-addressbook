fd5c0d91dbd40d7fee6209c5d02558a174c49aa4
Julie K <julieskim160@gmail.com>
2018-09-10 10:01:35 -0700

    add initial state with constructor and super props

--- src/components/address-book.js
+++ src/components/address-book.js
@@ -6,9 +6,30 @@ import Contact from './contact';
 
 export default class AddressBook extends React.Component {
     
+    constructor(props) {
+        super(props);
+        //initial state
+            this.state = {
+                contacts: [{
+                name: 'Alice Aardvark',
+                photo: 'https://api.adorable.io/avatars/64/alice%40thinkful.com',
+                address: '1600 Pennsylvania Ave'
+            }, {
+                name: 'Bob Bear',
+                photo: 'https://api.adorable.io/avatars/64/bob%40thinkful.com',
+                address: '725 5th Ave'
+            }, {
+                name: 'Carol Coyote',
+                photo: 'https://api.adorable.io/avatars/64/carol%40thinkful.com',
+                address: '4 Pennsylvania Plaza'
+            }]
+        }
+
+    }
+        
     //render an array Contact components corresponding to each person
     //this.state.contacts
     render() {
-        
+
     }
 }
