024287d4a7b0e2f4aed02825edc5d29db208d1f4
Julie K <julieskim160@gmail.com>
2018-09-10 13:25:59 -0700

    set up addressbook render

--- src/components/address-book.js
+++ src/components/address-book.js
@@ -9,43 +9,27 @@ import Contact from './contact';
 export default class AddressBook extends React.Component {
     constructor(props) {
         super(props);
-        //initial state
-            this.state = {
-                contacts: [{
-                    name: 'Alice Aardvark',
-                    photo: 'https://api.adorable.io/avatars/64/alice%40thinkful.com',
-                    address: '1600 Pennsylvania Ave'
-                }, {
-                    name: 'Bob Bear',
-                    photo: 'https://api.adorable.io/avatars/64/bob%40thinkful.com',
-                    address: '725 5th Ave'
-                }, {
-                    name: 'Carol Coyote',
-                    photo: 'https://api.adorable.io/avatars/64/carol%40thinkful.com',
-                    address: '4 Pennsylvania Plaza'
-                }]
-            }
-
+        this.state = {
+            contacts: [{
+                name: 'Alice Aardvark',
+                photo: 'https://api.adorable.io/avatars/64/alice%40thinkful.com',
+                address: '1600 Pennsylvania Ave'
+            }, {
+                name: 'Bob Bear',
+                photo: 'https://api.adorable.io/avatars/64/bob%40thinkful.com',
+                address: '725 5th Ave'
+            }, {
+                name: 'Carol Coyote',
+                photo: 'https://api.adorable.io/avatars/64/carol%40thinkful.com',
+                address: '4 Pennsylvania Plaza'
+            }]
+        }
     }
-        
-    //render an array Contact components corresponding to each person
-    //this.state.contacts
     render() {
-        // console.log(this.state.contacts)
-        const contacts = this.state.contacts.map((contact, index) => 
-            <Contact
-                key={index}
-                index={index} {...contact}
-            />
-            
-        );
-
-        console.log(contacts)
         return (
-            <div className="address-book">{contacts}</div>
-             
-
+            <div className="address-book">
+                {Contact}
+            </div>
         )
-        
     }
 }
